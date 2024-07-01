f49f815f65f940f59e86d4d01dff880e7af80695
Hendrik Weiß <git@hweiss.dev>
2024-07-01 10:23:22 +0200

    Filter als Lambda-Ausdruck

--- src/kt/java/range/RangeMain.java
+++ src/kt/java/range/RangeMain.java
@@ -4,17 +4,15 @@ import java.util.stream.Stream;
 
 public class RangeMain {
 
-    public static boolean istUngerade(int zahl) {
-        System.out.println("istUngerade");
-        return zahl % 2 != 0;
-    }
-
     public static void main(String[] args) {
 
 //        Stream<Integer> zahlen = StreamSupport.stream(Range.of(1,16, 3).spliterator(), false);
-        Stream.of(1,4,7,10,13)
-                .filter(RangeMain::istUngerade)
+        Stream.of(1,4,7,10,13,15)
+                .filter(zahl -> zahl % 2 != 0)
+                .skip(1)
                 .limit(2)
+                .map(zahl -> zahl * zahl)
+                .map(zahl -> 0.5 * zahl)
                 .forEach(System.out::println)
                 ;
 
