814775fb2e626322fd948864d45e436206b7caa8
Hendrik Weiß <git@hweiss.dev>
2024-07-01 09:52:54 +0200

    Filter als anonyme Klasse

--- src/kt/java/range/RangeMain.java
+++ src/kt/java/range/RangeMain.java
@@ -1,19 +1,9 @@
 package kt.java.range;
 
-import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 public class RangeMain {
-
-    public static class UngeradeFilter implements Predicate<Integer> {
-
-        @Override
-        public boolean test(Integer integer) {
-            return integer % 2 != 0;
-        }
-    }
 
     public static void main(String[] args) {
 //        for (int i : Range.of(1, 16, 3)) {
@@ -23,7 +13,12 @@ public class RangeMain {
 
 //        Stream<Integer> zahlen = StreamSupport.stream(Range.of(1,16, 3).spliterator(), false);
         long anzahl = Stream.of(1,4,7,10,13)
-                .filter(new UngeradeFilter())
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) {
+                        return integer % 2 != 0;
+                    }
+                })
                 .count();
 
         System.out.println(anzahl);
